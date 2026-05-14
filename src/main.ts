05280ced2e057614e8e2ba5b5eaf2fc54b301684
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2026-05-14 01:19:26 +0100

    feat: add Pino logging and improve server startup error handling

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,28 @@ import { config } from "@/config"
 
 const app = express()
 
-app.use(helmet())
-app.use(cookieParser())
-app.use(compression())
-app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.use(helmet());
+app.use(cookieParser());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
 
+(async () => {
+  try {
+    // await db.connect();
+    console.log('Database connected');
+    app.use("/",router)
+    
+    app.listen(config.port, () => {
+      console.log('Server is running');
+    });
+  } catch (error) {
+    console.error('Failed to start server', error);
+    if(config.nodeEnv === 'production') {
+      process.exit(1); // Exit with failure code in production
+    }
+  }
+})();
 
-app.listen(config.port,()=> {
-    console.log(`server is listening at http://localhost:${config.port}`)
-})
+
